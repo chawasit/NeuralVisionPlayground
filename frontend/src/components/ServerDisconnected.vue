1f2f7a69fa4ca954439a808e3a14b9c976c72dba
Chawasit Tengtrairatana <chawasit555@gmail.com>
2018-03-29 14:38:59 +0700

    display error message when socket disconnect

---
+++ frontend/src/components/ServerDisconnected.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container py-5">
+    <b-jumbotron header="Server disconnected" 
+                 lead="please make sure that server was running" 
+                 bg-variant="danger" 
+                 text-variant="white">
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServerDisconnected'
+}
+</script>
