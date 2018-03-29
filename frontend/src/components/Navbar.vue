f14956f3e9e00cf12f8431a7c8d1ba416f104741
Chawasit Tengtrairatana <chawasit555@gmail.com>
2018-03-29 14:10:50 +0700

    added bootstrap and socketio

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-navbar toggleable="md" type="dark" variant="primary">
+    <div class="container">
+      <b-navbar-brand href="#">Neural Vision Playground</b-navbar-brand>
+    </div>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
